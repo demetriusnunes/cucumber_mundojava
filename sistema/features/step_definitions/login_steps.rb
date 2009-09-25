d8711f2cd341afaed2a7d2b2b70977e056465454
demetriusnunes <demetriusnunes@gmail.com>
2009-09-25 01:16:25 -0300

    modificando para fazer uso das funcoes do swinger

--- sistema/features/step_definitions/login_steps.rb
+++ sistema/features/step_definitions/login_steps.rb
@@ -1,32 +1,25 @@
-import org.netbeans.jemmy.operators.JFrameOperator
-import org.netbeans.jemmy.operators.JDialogOperator
-import org.netbeans.jemmy.operators.JLabelOperator
-import org.netbeans.jemmy.operators.JTextFieldOperator
-import org.netbeans.jemmy.util.NameComponentChooser
-import org.netbeans.jemmy.operators.JButtonOperator
 import 'sistema.Login'
 
 Dado /^que estou na tela de Login$/ do
-  @container = JDialogOperator.new("Login")
+  @container = dialog("Login")
 end
 
 Quando /^preencho o campo "([^\"]*)" com "([^\"]*)"$/ do |campo, valor|
-  label = JLabelOperator.new(@container, campo)
-  field = JTextFieldOperator.new(label.label_for)
+  field = text_field(campo)
   field.text = valor
 end
 
 Quando /^aperto o botão "([^\"]*)"$/ do |botao|
-  button = JButtonOperator.new(@container, botao)
+  button = button(botao)
   button.push
 end
 
 Entao /^devo ver a tela Principal$/ do
-  @container = JFrameOperator.new("Principal")
+  @container = frame("Principal")
   assert @container.visible?
 end
 
 Entao /^devo ver a mensagem "([^\"]*)"$/ do |mensagem|
-  label = JLabelOperator.new(@container, mensagem)
+  label = label(mensagem)
   assert label.visible?
 end
