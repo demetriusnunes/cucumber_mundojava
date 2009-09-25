37d436b94adefe15fd36964df7943e2741e4079d
demetriusnunes <demetriusnunes@gmail.com>
2009-09-25 00:32:26 -0300

    adicionando Swinger e adequando documentacao e codigo

--- sistema/features/support/env.rb
+++ sistema/features/support/env.rb
@@ -1,6 +1,18 @@
 require 'test/unit'
-require File.dirname(__FILE__) + '/jemmy.jar'
-org.netbeans.jemmy.JemmyProperties.set_current_output org.netbeans.jemmy.TestOut.null_output
+
+begin
+  require File.dirname(__FILE__) + '/../../lib/swinger/lib/swinger'
+rescue LoadError
+  puts 'Swinger não está devidamente instalado. Veja o README.'
+  exit(-1)
+end
+
+begin
+  require File.dirname(__FILE__) + '/../../dist/sistema.jar'
+rescue LoadError
+  puts 'A aplicação precisa ser compilada. Veja o README.'
+  exit(-1)
+end
 
 World(Test::Unit::Assertions)
 
